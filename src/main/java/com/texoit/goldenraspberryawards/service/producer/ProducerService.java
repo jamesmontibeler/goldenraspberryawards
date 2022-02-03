80cc4e55fe32af146bc74b6d759505e9511c3963
James Perkison Montibeler <james.chico@gmail.com>
2022-02-03 08:53:34 -0300

    melhoria

--- src/main/java/com/texoit/goldenraspberryawards/service/producer/ProducerService.java
+++ src/main/java/com/texoit/goldenraspberryawards/service/producer/ProducerService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -30,13 +31,15 @@ public class ProducerService extends BaseCrudService<UUID, Producer, ProducerRep
 		if (years.size() < 2) {
 			return null;
 		}
+		
+		BiFunction<Integer,Integer, Integer> comparator = IntervalType.SHORTEST.equals(intervalType) ? Math::min : Math::max;
+		
 		aid.setPreviousWin(years.get(0));
 		aid.setFollowingWin(years.get(1));
 		aid.setInterval(aid.getFollowingWin() - aid.getPreviousWin());
 		for (int i = 2; i < years.size(); i++) {
 			Integer interval = (years.get(i) - years.get(i-1));
-			if ((IntervalType.SHORTEST.equals(intervalType) && interval < aid.getInterval())
-					|| (IntervalType.LONGEST.equals(intervalType) && interval > aid.getInterval())){
+			if (comparator.apply(interval, aid.getInterval()).equals(interval)){
 				aid.setPreviousWin(years.get(i-1));
 				aid.setFollowingWin(years.get(i));
 				aid.setInterval(interval);
@@ -54,9 +57,12 @@ public class ProducerService extends BaseCrudService<UUID, Producer, ProducerRep
 			List<Integer> years = repository.findYearOfProducerMovies(producer.getId().toString());
 			Optional.of(getInterval(aid, years, intervalType)).ifPresent(o -> awardIntervalSet.add(o));
 		}
+		
 		Function<List<Integer>, Integer> comparator = IntervalType.SHORTEST.equals(intervalType) ? Collections::min : Collections::max;
-		Integer shortestInterval = comparator.apply(awardIntervalSet.stream().map(AwardIntervalDetail::getInterval).collect(Collectors.toList()));
-		return awardIntervalSet.stream().filter(aid -> aid.getInterval() == shortestInterval).collect(Collectors.toSet());
+		
+		Integer interval = comparator.apply(awardIntervalSet.stream().map(AwardIntervalDetail::getInterval).collect(Collectors.toList()));
+		
+		return awardIntervalSet.stream().filter(aid -> aid.getInterval() == interval).collect(Collectors.toSet());
 	}
 	
 	
